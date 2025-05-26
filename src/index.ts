7ac0aec3e8bc125be66b383b0fc312eeeb60dd24
Leoxyzua <apinky549@gmail.com>
2025-05-26 18:35:40 -0500

    ok

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req: Request, res: Response) => {
-    res.send('¡Hola desde Express con TypeScript en Qoddi!');
+    res.send('alive');
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en puerto ${PORT}`);
+    console.log(`port ${PORT}`);
 });
