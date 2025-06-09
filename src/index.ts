9059a94714a9df7bde528b57afd1b0eb4fd7d8ad
Paulo Viteri <undevoted@proton.me>
2025-06-09 09:17:52 -0500

    hacking loeua

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import interactionsRouter from './bot'
 import { DataManager } from './lib'
 
 const port = process.env.PORT || 3000
+const development = process.env.DEVELOPMENT || false
 
 if (!process.env.MONGO_URI) {
   throw new Error('Missing environment variable MONGODB_URI')
@@ -19,6 +20,10 @@ express()
   .listen(port, () => {
     console.log(`Server is running on port ${port}`)
   })
+
+if (development) {
+  Bun.spawn(['bun', 'run', 'upload'], { cwd: "../" })
+}
 
 process.on('SIGINT', () => {
   console.log('shutting down hehehehehe\n')
