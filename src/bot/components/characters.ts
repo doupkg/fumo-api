ed7c47e725e073428737220aff40e9a0532d91a6
Paulo Viteri <undevoted@proton.me>
2025-06-10 16:55:21 -0500

    fix: i forgot to make an array a string

--- src/bot/components/characters.ts
+++ src/bot/components/characters.ts
@@ -4,6 +4,7 @@ import {
     APIMessageComponentSelectMenuInteraction,
     InteractionResponseType,
 } from 'discord-api-types/v10'
+import Characters from '@/data/characters.json'
 
 export const charactersComponent = {
     name: 'characters_input',
@@ -14,7 +15,14 @@ export const charactersComponent = {
         if (prev_fields!.find((e) => e.name === this.name)) {
             prev_fields!.pop()
         }
-        prev_fields!.push(Object({ name: this.name, value: interaction.data.values }))
+        prev_fields!.push(
+            Object({
+                name: this.name,
+                value: interaction.data.values
+                    .map((e) => Characters.find((x) => x.value === e)?.name)
+                    .join(', '),
+            }),
+        )
         return {
             type: InteractionResponseType.UpdateMessage,
             data: {
