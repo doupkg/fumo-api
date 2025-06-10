5329d586c988329a0e1ba2a9edb645b6bbd4ad1e
Paulo Viteri <undevoted@proton.me>
2025-06-10 13:53:58 -0500

    fix: probably a null value (its not happening, but ye)

--- src/bot/components/characters.ts
+++ src/bot/components/characters.ts
@@ -1,4 +1,5 @@
 import {
+    APIEmbedField,
     APIInteractionResponse,
     APIMessageComponentSelectMenuInteraction,
     InteractionResponseType,
@@ -9,18 +10,18 @@ export const charactersComponent = {
     async execute(
         interaction: APIMessageComponentSelectMenuInteraction,
     ): Promise<APIInteractionResponse> {
-        const prev_fields = interaction.message.embeds[0]?.fields ?? []
-        if (prev_fields.find((e) => e.name === this.name)) {
-            prev_fields.pop()
+        const prev_fields = interaction.message.embeds[0]?.fields ?? null
+        if (prev_fields!.find((e) => e.name === this.name)) {
+            prev_fields!.pop()
         }
-        prev_fields.push(Object({ name: this.name, value: interaction.data.values }))
+        prev_fields!.push(Object({ name: this.name, value: interaction.data.values }))
         return {
             type: InteractionResponseType.UpdateMessage,
             data: {
                 embeds: [
                     {
                         ...interaction.message.embeds[0],
-                        fields: prev_fields,
+                        fields: prev_fields as APIEmbedField[],
                     },
                 ],
             },
